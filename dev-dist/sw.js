2e65e6cad0d2e34934aa765ffc3bda2411478382
tal3nt3d <afonin05@yandex.ru>
2025-12-10 03:39:26 +0300

    added styles for approve&reject buttons

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ue6s72904co"
+    "revision": "0.4jfj5rp8nrg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
